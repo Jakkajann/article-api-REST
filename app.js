ce49b805df4583dcc3651a0e0f47ebc61e0685b0
Fernando Mocrosky <fernandobismaia@gmail.com>
2023-07-24 19:11:15 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set("view-engine", "ejs");
+app.set("view engine", "ejs");
 
 const articleSchema = {
   title: {
